a9d04a0aa2fa2a41e0df28513dde60b257a32120
Jiapan_Yu <2541771811@qq.com>
2020-08-28 18:13:48 +0800

    Vue 实例里面添加 el 选项 / DOM

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/styles/global.styl'
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-
 new Vue({
-  render(h) {
-    return h(App)
-  }
-}).$mount(root)
+  el: '#root',
+  template: '<div>this is content</div>',
+})
