e9577649a7e114d98160ea863c780be31600f975
Jiapan_Yu <2541771811@qq.com>
2020-09-02 16:05:09 +0800

    3章 Vue核心知识 -- Vue的组件之组件的定义  props 的一个单向数据流的概念

--- src/define/index.js
+++ src/define/index.js
@@ -4,11 +4,12 @@ const component = {
   props: {
     active: Boolean,
     propOne: String,
+    handleClick: Function,
   },
   template: `
     <div>
       <span v-show="active">see me if active</span>
-      <p>{{propOne}}</p>
+      <p @click="handleClick()">{{propOne}}</p>
     </div>
   `,
 };
@@ -19,8 +20,20 @@ new Vue({
   el: '#root',
   template: `
     <div>
-      <comp-one :active="true" propOne="some text"></comp-one>
+      <comp-one 
+        :active="true" 
+        :propOne="text"
+        :handleClick="this.handleClick"
+      ></comp-one>
       <comp-one :active="false"></comp-one>
     </div>
   `,
+  data: {
+    text: 'some text1',
+  },
+  methods: {
+    handleClick() {
+      this.text += '1'
+    },
+  },
 });
