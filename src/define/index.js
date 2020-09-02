2d1a37e61000a3e23d8c9584784a6a1852ae331f
Jiapan_Yu <2541771811@qq.com>
2020-09-02 11:44:37 +0800

    第3章 Vue核心知识 -- Vue的组件之组件的定义  props

--- src/define/index.js
+++ src/define/index.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
 
 const component = {
-  template: '<div>this is another component</div>'
+  props: {
+    active: Boolean,
+    propOne: String,
+  },
+  template: `
+    <div>
+      <span v-show="active">see me if active</span>
+      <p>{{propOne}}</p>
+    </div>
+  `,
 };
 
 Vue.component('CompOne', component);
 
 new Vue({
   el: '#root',
-  template: '<comp-one></comp-one>',
+  template: `
+    <div>
+      <comp-one :active="true" propOne="some text"></comp-one>
+      <comp-one :active="false"></comp-one>
+    </div>
+  `,
 });
