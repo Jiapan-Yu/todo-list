f254df47be196527b366fc69926af3cfcdb32e0d
Jiapan_Yu <2541771811@qq.com>
2020-09-03 15:09:18 +0800

    3章 Vue核心知识 -- Vue的组件之自定义双向绑定

--- src/practice/two-way-binding/v-model.js
+++ src/practice/two-way-binding/v-model.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 
 const component = {
+  props: ['value'],
   template: `
     <div>
-      <input type="text">
+      <input type="text" @input="handleInput" :value="value">
     </div>
-  `
+  `,
+  methods: {
+    handleInput(e) {
+      this.$emit('input', e.target.value)
+    },
+  }
 }
 
 new Vue({
@@ -13,9 +19,14 @@ new Vue({
     CompOne: component
   },
   el: '#root',
+  data() {
+    return {
+      value: '123',
+    };
+  },
   template: `
     <div>
-      <comp-one></comp-one>
+      <comp-one :value="value" @input="value = arguments[0]"></comp-one>
     </div>
   `
 })
