65b3a4b8e30d7eee9061b4870a12d762c17e94f0
Jiapan_Yu <2541771811@qq.com>
2020-09-04 15:06:15 +0800

    3章 Vue核心知识 -- Vue的组件之render function part 1 绑定事件

--- src/practice/render/render.js
+++ src/practice/render/render.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 
 const component = {
+  props: ['props1'],
   name: 'comp',
   render(createElement) {
     return createElement('div', {
-      style: this.style
-    }, this.$slots.default);
+      style: this.style,
+      on: {
+        click: () => { this.$emit('click') }
+      },
+    }, [
+      this.$slots.default,
+      this.props1,
+    ]);
   },
   data() {
     return {
@@ -30,13 +37,24 @@ new Vue({
     };
   },
   mounted() {
-    console.log()
+    console.log(this.$refs.comp.value)
+  },
+  methods: {
+    handleClick() {
+      console.log('clicked')
+    },
   },
   render(createElement) {
     return createElement(
       'comp-one',
       {
         ref: 'comp',
+        props: {
+          props1: this.value,
+        },
+        on: {
+          click: this.handleClick
+        },
       },
       // 字符串不需要用数组，子节点的时候需要用数组
       [
