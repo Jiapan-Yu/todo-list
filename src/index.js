767c0b14016dd2ece4992b538e4e26418412dbb1
Jiapan_Yu <2541771811@qq.com>
2020-09-11 09:58:27 +0800

    Vuex之集成支持服务端渲染

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './App.vue'
 import createRouter from './config/router'
-import store from './store/store'
+import createStore from './store/store'
 import './assets/styles/global.styl'
 
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const router = createRouter()
+const store = createStore()
 
 // 验证一些页面需要用户登陆才去显示的
 router.beforeEach((to, from, next) => {
