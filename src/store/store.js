7ff6327c839e3dfacb70c2dcdd762ce6b1c99657
Jiapan_Yu <2541771811@qq.com>
2020-09-11 17:13:56 +0800

    Vuex之state和getters actions的用处
    异步代码比如数据请求修改 store 里面的数据写到 actions 里面

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import defaultState from './state/state'
 import mutations from './mutations/mutations'
 import getters from './getters/getters.js'
+import actions from './actions/actions.js'
 
 export default () => {
   return new Vuex.Store({
@@ -11,5 +12,7 @@ export default () => {
     mutations,
 
     getters,
+
+    actions,
   })
 }
